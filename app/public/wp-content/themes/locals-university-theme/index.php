793f197386d9ef5a0e2e0e3d37d7037b8df3a498
David Garza <dgarza0413@gmail.com>
2020-08-28 18:35:33 -0500

    wordpress loop implemented with page and single files

--- app/public/wp-content/themes/locals-university-theme/index.php
+++ app/public/wp-content/themes/locals-university-theme/index.php
@@ -1,11 +1,8 @@
 <?php 
-    $names = array('Brad', 'John', "Meowsalot");
-    $count = 0;
-
-    while($count < count($names)) {
-        echo "<li>$names[$count]</li>";
-        $count++;
-    }
+  while(have_posts()){
+    the_post(); ?>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><h2>
+    <?php the_content(); ?>
+    <hr>
+  <?php }
 ?>
-
-<p>Hi, my name is <?php echo $names[1]; ?></p>
